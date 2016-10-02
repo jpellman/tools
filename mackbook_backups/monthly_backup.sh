639bdeaeb1abafed9e62b6207bdd28980cf905cf
John Pellman <pellman.john@gmail.com>
2016-10-02 00:03:30 -0400

    Scripts for backing up files on my Macbook

---
+++ mackbook_backups/monthly_backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the path to the Backup Volume.
+BACKUPVOL=/Volumes/BACKUP2
+
+# Use GNU's cp instead of the BSD version so that we can preserve hard links.
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+# Create the directory for today's backup.
+CUR_BACK=${BACKUPVOL}/$(date +"%m%d%y")
+LAST_BACK=${BACKUPVOL}/$(ls -t ${BACKUPVOL} | tail -1)
+
+# Make a recursive hard link directory for the last backup.
+cp -al ${LAST_BACK} ${CUR_BACK}
+
+# Select directories in my home to back up.
+BACKUP=(Documents Movies Music Pictures)
+
+for bakdir in ${BACKUP[@]};
+do
+   rsync -avu ${HOME}/${bakdir} ${CUR_BACK} --delete 
+done
